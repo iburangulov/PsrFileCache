495c9ec807187f528b67127ee45b4eed4535baf5
Burangulov <iburanguloff@ya.ru>
2021-05-30 15:50:19 +0500

    Работа с TTL

--- src/CacheClient.php
+++ src/CacheClient.php
@@ -12,188 +12,251 @@ final class CacheClient implements CacheInterface
     const SERVICE_FILE = '.metadoc';
 
     /**
-     * Директория хранения кеша
+     * Директория хранения кэша
      * @var string
      */
-    private $cachePath;
+    private $cachePath = __DIR__ . '/cache';
 
     /**
-     * Служебная информация о кэше
+     * Мета - данные о кэше
      * @var array
      */
-    private $meta = [];
+    private $metaData = [];
+
+    /**
+     * Временные данные
+     * @var array
+     */
+    private $temp;
+
+    /**
+     * Ранее загруженные данные
+     * @var array
+     */
+    private $loaded = [];
 
     /**
      * CacheClient constructor.
-     * @param string $cachePath
+     * @param string|null $cachePath Директория хранения кэша
      * @throws FileCacheException
      */
-    public function __construct(string $cachePath = __DIR__ . DIRECTORY_SEPARATOR . 'cache')
+    public function __construct(string $cachePath = null)
     {
-        $cachePath = rtrim($cachePath, DIRECTORY_SEPARATOR);
-        if (!is_dir($cachePath)) {
-            if (@!mkdir($cachePath)) throw new FileCacheException('Не удалось создать директорию хранения кэша');
-        }
-        if (!is_readable($cachePath)) throw new FileCacheException('Недостаточно прав для чтения кэша');
-        if (!is_writable($cachePath)) throw new FileCacheException('Недостаточно прав для записи кэша');
+        if ($cachePath) $this->cachePath = rtrim($cachePath, DIRECTORY_SEPARATOR);
 
-        if (file_exists($f = $cachePath . DIRECTORY_SEPARATOR . self::SERVICE_FILE)) {
+        if (!is_dir($this->cachePath)) {
+            if (@!mkdir($this->cachePath)) throw new FileCacheException('Не удалось создать директорию хранения кэша');
+        }
+        if (!is_readable($this->cachePath)) throw new FileCacheException('Недостаточно прав для чтения кэша');
+        if (!is_writable($this->cachePath)) throw new FileCacheException('Недостаточно прав для записи кэша');
+
+        if (file_exists($f = $this->cachePath . DIRECTORY_SEPARATOR . self::SERVICE_FILE)) {
             if (!is_readable($f)) throw new FileCacheException('Недостаточно прав для чтения служебного файла кэша');
-            $this->meta = json_decode(file_get_contents($f), true);
+            $this->metaData = json_decode(file_get_contents($f), true);
         }
 
         if (disk_free_space($cachePath) <= 8) throw new FileCacheException('Недостаточно места на диске для хранения кэша');
 
-        $this->cachePath = $cachePath;
+        $this->temp['set'] = [];
+        $this->temp['delete'] = [];
     }
 
     /**
-     * Получить значение по ключу
+     * Получить данные из кеша по ключу
      * @param string $key
      * @param null $default
-     * @return mixed|void
+     * @return mixed
      */
     public function get($key, $default = null)
     {
-        if (!$this->has($key)) return false;
+        if (!$this->has($key)) return $default;
 
-        $metaInformation = $this->getMetaData($key);
-        $data = file_get_contents($this->cachePath . DIRECTORY_SEPARATOR . sha1($key));
+        $k = sha1($key);
 
-        if (!$data) return $default;
-
-        if ($metaInformation)
+        if (array_key_exists($k, $this->temp['set'])) return $this->temp['set'][$k];
+        if (file_exists($this->getCacheFile($key)))
         {
-            if (isset($metaInformation['serialized']) && $metaInformation['serialized'] === true) $data = unserialize($data);
-            if (isset($metaInformation['ttl']) && isset($metaInformation['created']))
-            {
+            $data = file_get_contents($this->getCacheFile($key));
 
+            if (array_key_exists($k, $this->metaData))
+            {
+                if (array_key_exists('serializaed', $this->metaData[$k])
+                    && $this->metaData[$k]['serializaed']
+                )
+                    $data = unserialize($data);
+
+                if ($this->isKeyExpired($key)) return $default;
+
+                if (array_key_exists('type', $this->metaData[$k]))
+                {
+                    settype($data, $this->metaData[$k]['type']);
+                }
             }
+            $this->loaded[$k] = $data;
+            return $data;
         }
 
-        var_dump($data);
     }
 
     /**
-     * Установить значение для ключа
+     * Записать данные в кэш
      * @param string $key
      * @param mixed $value
      * @param null $ttl
-     * @return bool|void
+     * @return bool
      */
-    public function set($key, $value, $ttl = null)
+    public function set($key, $value, $ttl = null): bool
     {
-        //TODO Добавить поддержку DateInterval Object для TTL
-        $metaInformation = [];
         $k = sha1($key);
+        if (in_array($k, $this->temp['delete'])) unset($this->temp['delete'][$k]);
+
+        $meta = [];
+        $meta['type'] = gettype($value);
 
         if (is_array($value))
         {
             $value = serialize($value);
-            $metaInformation['serialized'] = true;
+            $meta['serialized'] = true;
         }
+
+        //TODO Добавить поддержку объектного представления TTL
+        if (is_object($ttl)) return false;
 
         if ($ttl && is_int($ttl))
         {
-            $metaInformation['ttl'] = $ttl;
-            $metaInformation['created'] = date('U');
+            $meta['ttl'] = $ttl;
+            $meta['created'] = date('U');
         }
 
-        if ($metaInformation) $this->meta[$k] = $metaInformation;
+        if ($meta) $this->metaData[$k] = $meta;
+        $this->temp['set'][$k] = $value;
 
-        file_put_contents($this->cachePath . DIRECTORY_SEPARATOR . $k, $value);
+        return true;
     }
 
     /**
-     * Удалить ключ и его значение
+     * Удалить данные из кэша по ключу
      * @param string $key
      * @return bool
      */
     public function delete($key): bool
     {
-        if ($this->has($key))
-        {
-            $k = sha1($key);
-            if (isset($this->meta[$k])) unset($this->meta[$k]);
-            return unlink($this->cachePath . DIRECTORY_SEPARATOR . $k);
-        }
-        return false;
+        if (!$this->has($key)) return false;
+
+        $k = sha1($key);
+
+        if (array_key_exists($k, $this->metaData)) unset($this->metaData[$k]);
+        if (array_key_exists($k, $this->temp['set'])) unset($this->temp['set'][$k]);
+        if (file_exists($f = $this->getCacheFile($key))) unlink($f);
+        return true;
     }
 
-    /**
-     * Очисть весь кэш
-     * @return bool|void
-     */
     public function clear()
     {
         // TODO: Implement clear() method.
     }
 
-    /**
-     * Получить несколько значений по ключам
-     * @param iterable $keys
-     * @param null $default
-     * @return iterable|void
-     */
     public function getMultiple($keys, $default = null)
     {
         // TODO: Implement getMultiple() method.
     }
 
-    /**
-     * Установить несколько значнений по ключам
-     * @param iterable $values
-     * @param null $ttl
-     * @return bool|void
-     */
     public function setMultiple($values, $ttl = null)
     {
         // TODO: Implement setMultiple() method.
     }
 
-    /**
-     * Удалить несколько ключей
-     * @param iterable $keys
-     * @return bool|void
-     */
     public function deleteMultiple($keys)
     {
         // TODO: Implement deleteMultiple() method.
     }
 
     /**
-     * Проверяет наличие ключа
+     * Проверить наличие ключа в кэше
      * @param string $key
      * @return bool
      */
     public function has($key): bool
     {
-        return file_exists($this->cachePath . DIRECTORY_SEPARATOR . sha1($key));
+        $k = sha1($key);
+        if (in_array($k, $this->temp['delete'])) return false;
+        elseif (array_key_exists($k, $this->temp['set'])) return true;
+        return file_exists($this->getCacheFile($key));
     }
 
     /**
-     * Получить мета - данные ключа
+     * Получить путь до кеш - файла на диске
      * @param $key
-     * @return array
+     * @return string
      */
-    private function getMetaData($key): array
+    private function getCacheFile($key, $alreadyCrypdedKey = false): string
     {
-        if (isset($this->meta[$k = sha1($key)]) && is_array($this->meta[$k])) return $this->meta[$k];
-        return [];
+        $k = $alreadyCrypdedKey ? $key : sha1($key);
+        return $this->cachePath . DIRECTORY_SEPARATOR . $k;
     }
 
     /**
-     * Обслуживание кэша
+     * Завершение работы кэша
      */
-    private function cleanUp(): void
+    private function shutdown(): void
     {
-        //TODO Сделать обслуживание
+        //Удаление просроченных ключей
+        foreach ($this->metaData as $k => $meta) {
+            if ($this->isKeyExpired($k, true))
+            {
+                unset($this->metaData[$k]);
+                if (array_key_exists($k, $this->temp['set'])) unset($this->temp['set'][$k]);
+                if (file_exists($f = $this->getCacheFile($k, true))) unlink($f);
+            }
+        }
+
+        //Сохранение мета - данных
+        if ($this->metaData)
+            file_put_contents($this->cachePath . DIRECTORY_SEPARATOR . self::SERVICE_FILE, json_encode($this->metaData));
+
+        //Сохранение данных
+        if ($this->temp['set'])
+        {
+            foreach ($this->temp['set'] as $k => $v) {
+                file_put_contents($this->getCacheFile($k, true), $v);
+            }
+        }
+
+        //Удаление данных
+        if ($this->temp['delete'])
+        {
+            foreach ($this->temp as $item) {
+                unlink($this->getCacheFile($item, true));
+            }
+        }
+    }
+
+    /**
+     * Проверка просроченности ключа по TTL
+     * @param string $key
+     * @param false $isCryptedKey
+     * @return bool true - просрочен, false - не просрочен
+     */
+    private function isKeyExpired(string $key, $isCryptedKey = false): bool
+    {
+        $k = $isCryptedKey ? $key : sha1($key);
+
+        if (array_key_exists($k, $this->metaData) &&
+            array_key_exists('ttl', $this->metaData[$k]) &&
+            array_key_exists('created', $this->metaData[$k])
+        ) {
+            $now = date('U');
+            $created = $this->metaData[$k]['created'];
+            $ttl = $this->metaData[$k]['ttl'];
+
+            if (($created + $ttl) < $now) return true;
+        }
+
+        return false;
     }
 
     public function __destruct()
     {
-        $this->cleanUp();
-        file_put_contents($this->cachePath . DIRECTORY_SEPARATOR . self::SERVICE_FILE,
-            $this->meta ? json_encode($this->meta) : '');
+        $this->shutdown();
     }
 }
